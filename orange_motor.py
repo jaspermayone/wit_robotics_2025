ee098f981c9f1d889eb4e613463805e4e4a1e183
Jasper Mayone <mayonej@wit.edu>
2025-11-18 20:16:27 -0500

    add ramping

--- orange_motor.py
+++ orange_motor.py
@@ -8,34 +8,24 @@ motor = PWMLED(14, frequency=50)
 # 0.05 (5%) = 1ms pulse = minimum throttle/stop
 # 0.075 (7.5%) = 1.5ms pulse = neutral
 # 0.10 (10%) = 2ms pulse = maximum throttle
-
 MIN_THROTTLE = 0.05
 MAX_THROTTLE = 0.10
 
-try:
-    # Arming sequence - send minimum throttle for 2 seconds
-    print("Arming ESC...")
-    motor.value = MIN_THROTTLE
-    sleep(2)
-
-    print("Running motor test...")
-    while True:
-        # Slowly increase throttle
-        print("Increasing speed...")
-        for throttle in range(50, 75, 5):  # From 5% to 7.5% in steps
-            motor.value = throttle / 1000
-            sleep(0.5)
-
-        # Hold at medium speed
-        sleep(2)
-
-        # Return to minimum
-        print("Decreasing speed...")
-        motor.value = MIN_THROTTLE
-        sleep(2)
-
-except KeyboardInterrupt:
-    print("Stopping motor...")
-    motor.value = MIN_THROTTLE
-    sleep(1)
-    motor.close()
+def ramp_throttle(start, end, duration=1.0, steps=50):
+    """
+    Smoothly ramp throttle from start to end value.
+    
+    Args:
+        start: Starting throttle value (0.05 - 0.10)
+        end: Ending throttle value (0.05 - 0.10)
+        duration: Total time for the ramp in seconds
+        steps: Number of intermediate steps
+    """
+    step_delay = duration / steps
+    step_size = (end - start) / steps
+    
+    current = start
+    for _ in range(steps):
+        current += step_size
+        motor.value = current
+        sleep(step_delay)
