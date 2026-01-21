bb54a9466d75d45c4a5f085c94d4d23dc7a3df21
Jasper Mayone <mayonej@wit.edu>
2026-01-20 19:27:57 -0500

    staged files

---
+++ src/config/secrets.h
@@ -0,0 +1,11 @@
+// =============================================================================
+// Secrets Configuration - DO NOT COMMIT TO GIT
+// =============================================================================
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi Access Point
+#define SECRET_WIFI_PASSWORD  "battlebot"  // Min 8 chars for WPA2
+
+#endif // SECRETS_H
