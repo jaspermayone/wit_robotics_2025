e596fc95cf7f4e85078b60988b75d5b467b2ba43
Jasper Mayone <mayonej@wit.edu>
2026-01-20 19:43:01 -0500

    fresh start for C

--- battlebot/main.py
+++
@@ -1,5 +0,0 @@
-# Bootstrap - adds src to path and runs the bot
-import sys
-sys.path.append('/src')
-
-exec(open('/src/main.py').read())
--- scratch/pico/tmp.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for f in os.listdir('/src'):
-      if f.endswith('.py'):
-          os.rename('/src/' + f, '/' + f)
